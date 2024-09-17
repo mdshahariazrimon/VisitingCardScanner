5ebb3e24568587c396f25e2835fb58f9595ee7e3
mdshahariazrimon <116777348+mdshahariazrimon@users.noreply.github.com>
2024-09-18 00:07:27 +0600

    Routes Added & Changed SDK version

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:visiting_card/pages/contact_details_page.dart';
+import 'package:visiting_card/pages/form_page.dart';
+import 'package:visiting_card/pages/home_page.dart';
+import 'package:visiting_card/pages/scan_page.dart';
+import 'package:visiting_card/providers/contact_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context)=> ContactProvider(),
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,10 +19,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
       ),
-      home: const Placeholder(),
+      initialRoute: HomePage.routename,
+      routes: {
+        HomePage.routename: (context)=> const HomePage(),
+        ScanPage.routename: (context)=> const ScanPage(),
+        FormPage.routename: (context)=> const FormPage(),
+        ContactDetailsPage.routename: (context)=> const ContactDetailsPage(),
+      },
     );
   }
 }
