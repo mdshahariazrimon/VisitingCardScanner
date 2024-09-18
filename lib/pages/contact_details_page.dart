15392685eb00ddd2cf7cde98c68239dcb602986a
mdshahariazrimon <116777348+mdshahariazrimon@users.noreply.github.com>
2024-09-19 02:12:31 +0600

    Call & SMS Button Created

--- lib/pages/contact_details_page.dart
+++ lib/pages/contact_details_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:visiting_card/models/contact_model.dart';
 import 'package:visiting_card/providers/contact_provider.dart';
+import 'package:visiting_card/utils/helpers.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   static const String routename='/details';
@@ -37,6 +39,68 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
                   Image.asset(contact.image,width: double.infinity,height: 250,fit: BoxFit.cover,),
                   ListTile(
                     title: Text(contact.name),
+                  ),
+                  ListTile(
+                    title: Text(contact.mobile),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: (){
+                            _smsContact(contact.mobile);
+                          },
+                          icon: const Icon(Icons.sms),
+                        ),
+                        IconButton(
+                          onPressed: (){
+                            _callContact(contact.mobile);
+                          },
+                          icon: const Icon(Icons.call),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(contact.email.isEmpty?'No Data':contact.email),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: (){
+                            _smsContact(contact.email);
+                          },
+                          icon: const Icon(Icons.email),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(contact.address.isEmpty?'No Data':contact.email),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: (){
+                            _smsContact(contact.address);
+                          },
+                          icon: const Icon(Icons.map),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(contact.website.isEmpty?'No Data':contact.email),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: (){
+                            _smsContact(contact.website);
+                          },
+                          icon: const Icon(Icons.web),
+                        ),
+                      ],
+                    ),
                   )
                 ],
               );
@@ -49,5 +113,25 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
         )
       ),
     );
+  }
+
+  void _smsContact(String mobile) async {
+    final url='sms: $mobile';
+    if(await canLaunchUrlString(url)){
+      await launchUrlString(url);
+    }
+    else{
+      showMsg(context, 'Can not perform this task');
+    }
+  }
+
+  void _callContact(String mobile) async{
+    final url='tel: $mobile';
+    if(await canLaunchUrlString(url)){
+    await launchUrlString(url);
+    }
+    else{
+    showMsg(context, 'Can not perform this task');
+    }
   }
 }
